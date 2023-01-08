891d56c69fffb5e55abce5225c4e81fd13a636c5
acsii-63 <duy.nq203690@sis.hust.edu.vn>
2023-01-08 16:59:34 +0700

    update auto detect

--- restart-4g.sh
+++ restart-4g.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-args=("$@")
-usb_port=${args[0]}
+dmesg | grep "Product: Dell" > detect_device.txt
 
-if [[ $usb_port == "" ]]; then
-    echo "RESTART when SIM7600G-H is disconnect to the Internet"
-    echo "Usage: sudo bash restart-4g.sh {usb_port}"
-    echo
-    echo "usb_port: USB port connect with SIM7600G-H 4G Module."
-else
-    while [[ 0 ]]; do
-        # ping -c 1 google.com &> /dev/null && echo "connected" || sudo bash -c "echo $usb_port > /sys/bus/usb/drivers/usb/bind"
-        if ping -c 1 google.com &> /dev/null
-        then
-            echo "connected"
-        else
-            sudo bash -c "echo $usb_port > /sys/bus/usb/drivers/usb/unbind"
-            sudo bash -c "echo $usb_port > /sys/bus/usb/drivers/usb/bind"
-        fi
-    done
-fi
+g++ -o usb-filter usb-filter.cpp
+
+./usb-filter
+
+usb_port=$(<usb_port.txt)
+
+while [[ 0 ]]; do
+    if ping -c 1 google.com &> /dev/null
+    then
+        echo "connected"
+    else
+        sudo bash -c "echo $usb_port > /sys/bus/usb/drivers/usb/unbind"
+        sudo bash -c "echo $usb_port > /sys/bus/usb/drivers/usb/bind"
+    fi
+done
